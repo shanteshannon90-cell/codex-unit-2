44f55c8d2ba0664f2494e3ffdad871da21d2d666
Shante Shannon <shante.shannon90@gmail.com>
2026-04-27 22:49:00 -0400

    completed level 8

--- lesson-04-js-objects/level-08/nested.js
+++ lesson-04-js-objects/level-08/nested.js
@@ -1,5 +1,15 @@
 // TODO: Default-export an object with nested properties.
 // Example: export default { theme: { color: 'blue' } }
-export default {
-  /* TODO: nested structure */
+
+/* TODO: nested structure */
+const lifeDashboard = {
+  firstname: "Shante",
+  lastname: "Shannon",
+  age: 35,
+  address: {
+    city: "Statesville",
+    state: "North Carolina",
+  },
 };
+
+export default lifeDashboard.address;
