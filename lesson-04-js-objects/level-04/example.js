90b4bbbbe620043fef1c31e9b598b1d080a8c30f
Shante Shannon <shante.shannon90@gmail.com>
2026-04-27 22:28:20 -0400

    complteted level 4

--- lesson-04-js-objects/level-04/example.js
+++ lesson-04-js-objects/level-04/example.js
@@ -1,5 +1,11 @@
 // TODO: Default-export an object literal using literal syntax.
 // Example: export default { a: 1, b: "x" };
-export default {
-  /* TODO: add keys and values */
+/* TODO: add keys and values */
+const lifeDashboard = {
+  firstname: "Shante",
+  lastname: "Shannon",
+  age: 35,
+  occupation: "student",
 };
+
+export default lifeDashboard;
