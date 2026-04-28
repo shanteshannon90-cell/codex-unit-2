b3342f8d8e394ef65825fdb1a9b229e8e1624dd7
Shante Shannon <shante.shannon90@gmail.com>
2026-04-27 22:13:37 -0400

    completed level 1

--- lesson-04-js-objects/level-01/description.js
+++ lesson-04-js-objects/level-01/description.js
@@ -1,3 +1,3 @@
 // TODO: Default-export a short description string about objects.
 // Example: export default "Objects are key/value collections."
-export default "TODO: A short description of objects.";
+export default "A JavaScript object is a collection of related data and behavior, stored together in one container and is made of properties and methods.";
