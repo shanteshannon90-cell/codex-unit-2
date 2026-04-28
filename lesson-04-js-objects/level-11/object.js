086e3ca933e7d9d119d1a3fed245828e74445479
Shante Shannon <shante.shannon90@gmail.com>
2026-04-27 23:25:31 -0400

    completed level 11

--- lesson-04-js-objects/level-11/object.js
+++ lesson-04-js-objects/level-11/object.js
@@ -1,6 +1,15 @@
 // TODO: Build an object from a written specification and export it as default.
 // Example spec: book with title, author, pages, and a summary() method assigned from a named function.
+function summary() {
+  return this.title + this.author;
+}
 const book = {
   /* TODO: implement spec */
+  title: "Harry Potter and the Sorcerer's Stone",
+  author: "J.K Rowling",
+  pages: 309,
 };
+
+book.summary = summary;
+
 export default book;
