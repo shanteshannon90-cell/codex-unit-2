0c24168e37887b4172e4e50970cb2c8b30cfb498
Shante Shannon <shante.shannon90@gmail.com>
2026-04-27 22:12:48 -0400

    completed level 1

--- lesson-04-js-objects/level-01/examples.js
+++ lesson-04-js-objects/level-01/examples.js
@@ -1,3 +1,3 @@
 // TODO: Default-export an array of two short example strings.
 // Example: export default ["{ a: 1 }", "{ name: 'Ava' }"]
-export default ["TODO example 1", "TODO example 2"];
+export default ["apple", "banana", "grapes"];
