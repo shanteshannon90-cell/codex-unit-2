4a516e10ee7a1787b75c3a02426f5cfcb4c947e3
Shante Shannon <shante.shannon90@gmail.com>
2026-04-27 22:19:45 -0400

    added object properties

--- lesson-04-js-objects/level-03/object.js
+++ lesson-04-js-objects/level-03/object.js
@@ -1,6 +1,9 @@
 // TODO: Declare a simple object and export it as the default export.
 // Example: const user = { name: 'Ava' }; export default user;
 const studentObject = {
+  firstname: "Shante",
+  lastname: "Shannon",
+  age: 35,
   /* TODO: add properties */
 };
 export default studentObject;
