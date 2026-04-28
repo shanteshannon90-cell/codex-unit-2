6894621008c763b3af4ad8f77c9a950ead1ccb9d
Shante Shannon <shante.shannon90@gmail.com>
2026-04-27 22:32:21 -0400

    completed level 5

--- lesson-04-js-objects/level-05/access.js
+++ lesson-04-js-objects/level-05/access.js
@@ -1,3 +1,10 @@
 // TODO: Demonstrate dot-notation property access and default-export the result/example.
 // Example: const user = { name: 'Ava' }; export default user.name;
-export default null;
+const lifeDashboard = {
+  firstname: "Shante",
+  lastname: "Shannon",
+  age: 35,
+  occupation: "student",
+};
+
+export default lifeDashboard.age;
