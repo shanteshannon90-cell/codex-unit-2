2ecb8534932b43dc0e442e01c6c25d1a31735145
Shante Shannon <shante.shannon90@gmail.com>
2026-05-12 18:11:50 -0400

    modified level

--- lesson-08-rest-api/level-09/script.js
+++ lesson-08-rest-api/level-09/script.js
@@ -1,4 +1,5 @@
 // Level 09 starter script
 // TODO: Save a target element and set its innerText to briefly compare GET and POST.
 const result = document.getElementById("result");
-// Example: result.innerText = 'GET is for reading; POST is for sending data.'
+result.innerText =
+  "GET method pulls and reads data from the server, POST method sends the data";
