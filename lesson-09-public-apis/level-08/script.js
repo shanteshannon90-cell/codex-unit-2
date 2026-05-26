d9861ca360678a6e4120f401e4d89f8492fd564c
Shante Shannon <shante.shannon90@gmail.com>
2026-05-25 22:51:35 -0400

    modified index and js

--- lesson-09-public-apis/level-08/script.js
+++ lesson-09-public-apis/level-08/script.js
@@ -1,9 +1,25 @@
 const formTag = document.getElementById("jokeForm");
 formTag.onsubmit = handleSubmit;
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
-  // TODO: Create a `data` object with an "api-key" property (quoted when hyphenated)
-  // TODO: Convert `data` into a query string and fetch jokes (or send as header if API requires)
-  // TODO: Parse response and render a joke into the DOM. On invalid key, show an error message in the page
+
+  const data = {
+    "api-key": "89b3b872d87c4f7ea0cd73055dc305be",
+  };
+
+  const queryString = new URLSearchParams(data);
+
+  const response = await fetch(
+    "https://api.humorapi.com/memes/random" + "?" + queryString,
+  );
+  const result = await response.json();
+
+  const description = result.description;
+  const src = result.url;
+  const imageTag = document.getElementById("image");
+  const outputTag = document.getElementById("display");
+
+  outputTag.innerText = description;
+  imageTag.src = src;
 }
